10464f966e21ab132de664eb115f553d62b59a3b
Abhishek Guleri <43719098+abhishekguleri@users.noreply.github.com>
2019-03-06 22:40:02 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
-import javax.swing.JOptionPane;
-public class Main
-{
-    public static void main(String[] args){
-        String name = JOptionPane.showInputDialog("how are you");
-        String message =String.format("welcome ,%s,friends",name);
+public class NumberPalindrome {
+    public static boolean isPalindrome(int number){
+       int temp=number;
+    int reverse =0;
+while(number!=0){int lastDigit=number%10;
+        reverse=reverse*10;
+        reverse =reverse+lastDigit;
+        number=number/10;
     }
+        if(temp==reverse){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+ 
 }
